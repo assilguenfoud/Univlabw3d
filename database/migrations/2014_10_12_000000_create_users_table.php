84c04891a0a8639c452fe2b8a13a5e2fe6674e9e
Assil Guenfoud <assilguenfoud@users.noreply.github.com>
2022-11-10 23:37:07 +0100

    Laravel Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,11 +14,16 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->smallIncrements('id');
+            $table->string('username');
+            $table->string('password');
+            $table->string('inscription_number');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->dateTime('creation_date');
+            $table->dateTime('update_date');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -33,4 +38,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
     }
-};
+}
