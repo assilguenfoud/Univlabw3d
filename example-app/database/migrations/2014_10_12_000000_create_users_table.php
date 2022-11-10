76cf512788ad106d383861c2abfe01a8615119e2
Assil Guenfoud <assilguenfoud@users.noreply.github.com>
2022-11-10 23:26:29 +0100

    add faissal's changes

---
+++ example-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('username');
+            $table->string('password');
+            $table->string('inscription_number');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->dateTime('creation_date');
+            $table->dateTime('update_date');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
